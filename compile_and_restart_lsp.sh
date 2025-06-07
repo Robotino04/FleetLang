aba0a787fae370e7f07fa6eebf7f78bd6585e824
Robotino04 <100292446+Robotino04@users.noreply.github.com>
2025-06-07 15:28:32 +0200

    Update compile_and_restart_lsp to not change user state

--- compile_and_restart_lsp.sh
+++ compile_and_restart_lsp.sh
@@ -3,15 +3,18 @@
 function restart_lsp() {
     for file in "$XDG_RUNTIME_DIR/nvim."*; do
         echo "restarting instance $file"
-        nvim --server $file --remote-send "<Esc><Esc>:source load.vim | LspStop fleetls<CR>" --headless
-        nvim --server $file --remote-send "<Esc><Esc>:LspStart fleetls<CR>" --headless
+        nvim --server $file --remote-expr 'execute("source load.vim")' --headless
+        nvim --server $file --remote-expr 'execute("LspStop fleetls")' --headless
+        nvim --server $file --remote-expr 'execute("LspStart fleetls")' --headless
     done
 }
 
-(cargo run --bin fleetls --color=always 2>&1 || (echo "\033[31m!!Starting last successful build!!\033[0m" && ./target/debug/fleetls & restart_lsp)) | while IFS= read -r line; do
+(cargo run --bin fleetls --color=always 2>&1 || (
+    echo "\033[31m!!Starting last successful build!!\033[0m" && ./target/debug/fleetls &
+    restart_lsp
+)) | while IFS= read -r line; do
     echo -e "$line"
     if [[ "$line" == *"Running"* ]]; then
         restart_lsp
     fi
 done && echo "FleetLS exited"
-
